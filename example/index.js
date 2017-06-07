9f93c7b369b5ba3e1458ba41703a302527fa8cce
bredele <olivier.wietrich@gmail.com>
2017-06-07 13:15:39 -0600

    Setup react environment

---
+++ example/index.js
@@ -0,0 +1,16 @@
+/**
+ * Dependencies.
+ */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Badge from '..'
+
+/**
+ * Render badge.
+ */
+
+ReactDOM.render(
+  <Badge id="id1208564443"/>,
+  document.querySelector('main')
+)
