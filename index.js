684c19a216a3889de6e121ca34bd7b84929285ab
bredele <olivier.wietrich@gmail.com>
2017-06-14 20:03:57 -0600

    Add title and badge label

--- index.js
+++ index.js
@@ -8,14 +8,16 @@ import React from 'react'
 /**
  * This is a simple description.
  *
+ * @param {Object} props
  * @api public
  */
 
 export default (props) => {
   //itms-apps
+  const title = props.title || 'Download Android application'
   return (
-    <a href={'https://play.google.com/store/apps/details?id=' + props.id}>
-      <img src="https://tether.github.io/react-download-android/badge.png"></img>
+    <a href={'https://play.google.com/store/apps/details?id=' + props.id} aria-label={title}>
+      <img src="https://tether.github.io/react-download-android/badge.png" title={title}></img>
     </a>
   )
 }
